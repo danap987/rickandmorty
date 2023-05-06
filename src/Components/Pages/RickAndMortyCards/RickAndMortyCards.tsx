c7b751d513c29a0eabcb8c45245af8093eb01601
danap987 <srahavd@gmail.com>
2023-05-06 11:30:51 +0300

    select

--- src/Components/Pages/RickAndMortyCards/RickAndMortyCards.tsx
+++ src/Components/Pages/RickAndMortyCards/RickAndMortyCards.tsx
@@ -6,6 +6,7 @@ import ShowCard from "../ShowCard/ShowCard";
 
 function RickAndMortyCards(): JSX.Element {
     const [results, setResults] = useState<ResultModel[]>([]);
+    const [filterCard, setFilterCard] = useState<string> (``);
 
     useEffect(() => {
         console.log(123);
@@ -17,7 +18,21 @@ function RickAndMortyCards(): JSX.Element {
 
     return (
         <div className="RickAndMortyCards">
-			{results.map(m=><ShowCard key={`mshow`+m.id} show={m}/>)}
+
+<label>
+      Pick a Genre:
+      <select name="selectedGenre" defaultValue=" "  
+          onChange={event => setFilterCard(event.target.value)}>
+        <option value="">All</option>
+        <option value="Alien">species-Alien</option>
+        <option value="Human">species- Human</option>
+        <option value="Unknown">species/status-Unknown</option>
+        <option value="Alive">status-Alive</option>
+        <option value="Dead">status-dead</option>
+    
+      </select>
+    </label>
+			{results.filter(f => f.species.includes(filterCard) || f.status.includes(filterCard) || filterCard === '').map(f=><ShowCard key={`mshow`+f.id} show={f}/>)}
         </div>
     );
 }
