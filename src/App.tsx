eb3a35e5746db6b9f98c52bf9f0df9a6f8fec9f5
danap987 <srahavd@gmail.com>
2023-05-11 20:01:46 +0300

    light-dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Layout/Header/Header';
@@ -6,10 +6,25 @@ import Menu from './Components/Layout/Menu/Menu';
 import Main from './Components/Layout/Main/Main';
 import Footer from './Components/Layout/Footer/Footer';
 
+
+type Theme = 'light-mode' | 'dark-mode';
+
 function App() {
-  return (
-    <div className="App">
-     
+
+    const [theme, setTheme] = useState<Theme>('light-mode');
+
+    const changeTheme = () => {
+        if (theme === 'light-mode') {
+            setTheme('dark-mode');
+        } else {
+            setTheme('light-mode');
+        }
+    }
+    return (
+        <div className={`App ${theme}`}>
+            <button onClick={changeTheme}>
+                {theme === 'light-mode' ? <span>🌚</span> : <span>🌝</span>}
+     </button>
      <Header/>
      <Menu/>
      <Main/>
